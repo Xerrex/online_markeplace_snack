8b77b286bfc6f42fc909a1b518355cf8a08549c3
Alex Kagai <alexkagai@outlook.com>
2023-12-03 17:28:42 +0300

    Add Items searching

--- mkt_item/urls.py
+++ mkt_item/urls.py
@@ -6,6 +6,7 @@ app_name = "mkt_item"
 
 
 urlpatterns = [
+    path("", views.browse, name="browse"),
     path("new/", views.new_mkt_item, name="new"),
     path("<int:pk>/", views.detail, name="detail"),
     path('<int:pk>/edit/', views.edit, name='edit'),
